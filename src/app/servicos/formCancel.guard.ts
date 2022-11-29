6c1525fb41a74177941242620857f88fb45b13c6
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-11-29 16:25:06 -0300

    guard

---
+++ src/app/servicos/formCancel.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormCancelGuard implements CanDeactivate<unknown> {
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      console.log("====[CanDeactivate]=====")
+
+      return confirm("Deseja realmente sair do formulário?");
+  }
+  
+}
